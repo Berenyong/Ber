2ae8e1018ac3e39114b0ec4d1fbabcbcd072e196
최원용 <nyong@choewon-yong-ui-MacBookPro.local>
2022-08-13 23:34:55 +0900

    FEAT :: user domain

--- src/main/java/bssm/major/club/ber/BerApplication.java
+++ src/main/java/bssm/major/club/ber/BerApplication.java
@@ -2,7 +2,9 @@ package bssm.major.club.ber;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class BerApplication {
 
