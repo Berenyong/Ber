eb29cfa859b9fb218ecb9cc87f85ac7ef0f2c2a4
최원용 <nyong@choewon-yong-ui-MacBookPro.local>
2022-08-14 03:22:55 +0900

    FEAT :: 로그아웃

--- src/main/java/bssm/major/club/ber/domain/user/web/api/AuthApiController.java
+++ src/main/java/bssm/major/club/ber/domain/user/web/api/AuthApiController.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
 @RequiredArgsConstructor
@@ -22,4 +23,8 @@ public class AuthApiController {
         return authService.login(request);
     }
 
+    @DeleteMapping("/logout")
+    public void logout(HttpServletRequest request) {
+        authService.logout(request.getHeader("ACCESS-TOKEN"));
+    }
 }
