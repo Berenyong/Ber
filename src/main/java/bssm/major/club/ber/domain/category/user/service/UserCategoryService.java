033ea93cf15037c388585988ad16d0af0c2c438f
wonyongChoi05 <rltgjqmduftlagl@gmail.com>
2022-09-15 00:17:33 +0900

    FEAT :: UserCategoryService

---
+++ src/main/java/bssm/major/club/ber/domain/category/user/service/UserCategoryService.java
@@ -0,0 +1,39 @@
+package bssm.major.club.ber.domain.category.user.service;
+
+import bssm.major.club.ber.domain.category.user.domain.UserCategory;
+import bssm.major.club.ber.domain.category.user.repository.UserCategoryRepository;
+import bssm.major.club.ber.domain.user.domain.User;
+import bssm.major.club.ber.domain.user.domain.repository.UserRepository;
+import bssm.major.club.ber.global.config.security.SecurityUtil;
+import bssm.major.club.ber.global.exception.CustomException;
+import bssm.major.club.ber.global.exception.ErrorCode;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+@Service
+public class UserCategoryService {
+
+    private final UserRepository userRepository;
+    private final UserCategoryRepository userCategoryRepository;
+
+    @Transactional
+    public Long createCategory(String category) {
+        User user = userRepository.findByEmail(SecurityUtil.getLoginUserEmail())
+                .orElseThrow(() -> new CustomException(ErrorCode.USER_NOT_LOGIN));
+
+        UserCategory userCategory = userCategoryRepository.save(
+                UserCategory.builder()
+                        .name(category)
+                        .build()
+        );
+
+        userCategory.confirmUser(user);
+
+        return user.getId();
+    }
+}
